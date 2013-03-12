f361e119093605282ad088d016a601b3f73cd8d8
Rob Rodi <robert.rodi@gmail.com>
2013-03-11 21:54:42 -0700

    better base

---
+++ Progression/AsyncHelpers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using NLog;
+namespace Progression
+{
+    public static class AsyncHelpers
+    {
+        public static IEnumerable<TEntity> QueryAll<TEntity>(this CloudTable table, string filter) where TEntity : AuditedEntityBase<TEntity>, new()
+        {
+            LogManager.GetCurrentClassLogger().Info("Query Filter: {0}", filter);
+            var query = new TableQuery<TEntity>().Where(filter);
+            TableQuerySegment<TEntity> currentSegment = null;
+
+            while (currentSegment == null || currentSegment.ContinuationToken != null)
+            {
+                var token = currentSegment != null ? (TableContinuationToken)currentSegment.ContinuationToken : null;
+                currentSegment = table.ExecuteQuerySegmented(query, token);
+                foreach (var item in currentSegment.Results)
+                {
+                    yield return item;
+                }
+            }
+            //Task.Factory.FromAsync<ResultSegment<TEntity>>(, table.EndExecuteQuerySegmented<TEntity>, query, null, null);
+        }
+        public static async Task<bool> AsyncCreateIfNotExist(this CloudTable table)
+        {
+            return await Task.Factory.FromAsync<bool>(table.BeginCreateIfNotExists, table.EndCreateIfNotExists, null);
+        }
+        public static async Task<TEntity> GetSingle<TEntity>(this CloudTable table, string pk, string rk) where TEntity : AuditedEntityBase<TEntity>, new()
+        {
+            return (await table.AsyncExecute(TableOperation.Retrieve<TEntity>(pk, rk))).Result as TEntity;
+        }
+        public static async Task<TableResult> AsyncExecute(this CloudTable table, TableOperation operation)
+        {
+            return await Task.Factory.FromAsync<TableOperation, TableResult>(table.BeginExecute, table.EndExecute, operation, null);
+        }
+    }
+
+}
---
+++ Progression/TableName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Progression
+{
+    public class TableNameAttribute : Attribute
+    {
+        public string Name { get; private set; }
+        public TableNameAttribute(string name) { Name = name; }
+    }
+}
